579bb6d3dfe3031ba7e66c56278a2e62377d3d01
VictorCasCat <vcascat@upv.edu.es>
2025-12-24 15:15:00 +0100

    Point

---
+++ Point2D.h
@@ -0,0 +1,21 @@
+#ifndef POINT2D_H
+#define POINT2D_H
+
+#include <ostream>
+using namespace std;
+
+class Point2D{
+	public:
+		double x;
+		double y;
+
+		Point2D(double x = 0, double y = 0);
+
+		static double distance(const Point2D &a, const Point2D &b);
+
+		friend bool operator==(const Point2D &a, const Point2D &b);
+		friend bool operator!=(const Point2D &a, const Point2D &b);
+		friend ostream& operator<<(ostream &out, const Point2D &p);
+};
+
+#endif
