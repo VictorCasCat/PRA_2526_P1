2bf9573c35ed2dee0d05b33d62b8c95b6ce04ce6
VictorCasCat <vcascat@upv.edu.es>
2026-01-05 16:36:49 +0100

    Shape

--- Shape.h
+++ Shape.h
@@ -22,7 +22,7 @@ class Shape{
 		void set_color(string c);
 
 		virtual double area() const = 0;
-		virtual double preimetro() const = 0;
+		virtual double perimeter() const = 0;
 		virtual void translate(double incX, double incY) = 0;
 		virtual void print() = 0;
 };
